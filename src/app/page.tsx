d59b4b012ad454c3e2eb3d865dca002f3309a695
xBookbig <koonbookza2015@gmail.com>
2024-05-11 16:19:01 +0700

    Change () style web

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Todolist from "@/components/todolist";
 
 export default function Home() {
   return (
-    <div className="container mx-auto">
-      <h1 className="text-2xl font-semibold mb-4">Auto Delete Todo List</h1>
+    <div className="w-full flex flex-col items-center justify-center min-h-screen py-2 bg-gray-200">
+      <h1 className=" text-black font-bold mb-8">Auto Delete Todo List</h1>
       <Todolist/>
     </div>
   );
