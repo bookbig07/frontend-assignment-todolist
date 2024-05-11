70dfe8ced8733b6022eb99272bb22e1e128e9d06
xBookbig <koonbookza2015@gmail.com>
2024-05-11 15:25:07 +0700

    add () init value

--- src/components/todolist.tsx
+++ src/components/todolist.tsx
@@ -1,6 +1,68 @@
+"use client"
+
 import { useState, useEffect } from 'react';
 
+type typeList = {
+  type: string;
+  name: string;
+};
+
+const valueList: typeList[] = [
+  {
+    type: 'Fruit',
+    name: 'Apple',
+  },
+  {
+      type: 'Vegetable',
+      name: 'Broccoli',
+  },
+  {
+      type: 'Vegetable',
+      name: 'Mushroom',
+  },
+  {
+      type: 'Fruit',
+      name: 'Banana',
+  },
+  {
+      type: 'Vegetable',
+      name: 'Tomato',
+  },
+  {
+      type: 'Fruit',
+      name: 'Orange',
+  },
+  {
+      type: 'Fruit',
+      name: 'Mango',
+  },
+  {
+      type: 'Fruit',
+      name: 'Pineapple',
+  },
+  {
+      type: 'Vegetable',
+      name: 'Cucumber',
+  },
+  {
+      type: 'Fruit',
+      name: 'Watermelon',
+  },
+  {
+      type: 'Vegetable',
+      name: 'Carrot',
+  },
+];
+
+const itemList = {
+  items: valueList,
+  fruitList: [],
+  vegetableList: [],
+};
+
 export default function Todolist() {
+  const [state, setState] = useState(itemList);
+
   return (
     <div>todolist</div>
   )
