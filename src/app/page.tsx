fdff96685454b4be5810bb25b61ef1b1c8592591
xBookbig <koonbookza2015@gmail.com>
2024-05-11 16:57:23 +0700

    remove () text black

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Todolist from "@/components/todolist";
 export default function Home() {
   return (
     <div className="w-full flex flex-col items-center justify-center min-h-screen py-2 bg-gray-200">
-      <h1 className=" text-black font-bold text-[#016BA1] text-5xl	mb-6">Auto Delete Todo List</h1>
+      <h1 className="font-bold text-[#016BA1] text-5xl	mb-6">Auto Delete Todo List</h1>
       <Todolist/>
     </div>
   );
